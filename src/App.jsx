0ececc5a4ea818ef1549d3cd919a2dfbff2e9ccd
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-10-04 12:11:22 +0200

    feat: add minor components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import './App.css'
+import data from '../data.json'
+import { useState } from 'react'
+import TodoList from './components/TodoList'
 
 function App() {
-  return <></>
+  const [todos, setTodos] = useState(data)
+
+  return (
+    <>
+      <TodoList todos={todos} />
+    </>
+  )
 }
 
 export default App
---
+++ src/components/TodoItem.jsx
@@ -0,0 +1,3 @@
+export default function TodoItem({ todo }) {
+  return <div>{todo.name}</div>
+}
