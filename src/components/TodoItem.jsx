722b5decb065147c834bf93125f931564b5130c4
Gorka Diez <99983319+godidev@users.noreply.github.com>
2023-10-04 12:51:36 +0200

    feat: add checkbox to todos

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,8 +1,15 @@
+import './TodoItem.css'
+
 export default function TodoItem({ todo }) {
   return (
-    <div>
-      <h3>{todo.name}</h3>
-      <p>{todo.description}</p>
+    <div className="todo-item">
+      <div>
+        <input type="checkbox" checked={todo.completed} />
+      </div>
+      <div>
+        <h3>{todo.name}</h3>
+        <p>{todo.description}</p>
+      </div>
     </div>
   )
 }
